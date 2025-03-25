8e3ce186bfaf0f731a1bd5313c93395a41e55f02
roshansrini26 <roshansrini26@gmail.com>
2025-03-25 17:36:17 +0530

    base

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return "<h1>Logout</h1>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up </p>"
+    return render_template("sign_up.html")
 
